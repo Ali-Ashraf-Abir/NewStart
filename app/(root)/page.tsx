944689c864a2f897cf676119dfdd86b44c6cef6b
Ali Ashraf Abir <fazilabir88@gmail.com>
2025-01-24 14:55:40 +0600

    added forms and search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,29 @@
+import { SearchParams } from "next/dist/server/request/search-params";
+import SearchForm from "../components/SearchForm";
 
 
 
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const query=(await searchParams).query
+  
+  
+
   return (
        <>
         <section className="pink_container">
         <h1 className="heading">Pitch your startup,<br></br>Connect With Entrepreneuers</h1>
       <p className="sub-heading !max-w-3xl">Submit Ideas,Vote On Pitches,and Get Noticed in Virtual competition</p>
+      <SearchForm query={query}></SearchForm>
+      </section>
+
+      <section className="section_container">
+        <p className="text-nunito text-30-semibold">
+      {query?`Search Result for "${query}"`:'All startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+
+        </ul>
       </section>
        </>
   );
---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Form from 'next/form'
+import SearchFormReset from './SearchFormReset'
+import { Search } from 'lucide-react'
+
+async function SearchForm({query}:{query?:string}) {
+
+  
+  
+
+
+  return (
+    <Form action="/" scroll={false} className='search-form'>
+      
+      <input 
+      name='query'
+      defaultValue={query}
+      className='search-input'
+      placeholder='Search Startups'></input>
+
+      <div className="flex gap-2">
+        {query&&(
+          <SearchFormReset></SearchFormReset>)}
+
+          <button type='submit' className='search-btn text-white'><Search></Search></button>
+      </div>
+      
+      </Form>
+  )
+}
+
+export default SearchForm
