6ce6e2476b365adc3c7da59786a2baa6e1532e7a
Ali Ashraf Abir <fazilabir88@gmail.com>
2025-01-24 15:23:22 +0600

    added cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import { SearchParams } from "next/dist/server/request/search-params";
 import SearchForm from "../components/SearchForm";
+import StartupCard from "../components/StartupCard";
 
 
 
@@ -7,7 +8,18 @@ import SearchForm from "../components/SearchForm";
 export default async function Home({ searchParams }) {
   const query=(await searchParams).query
   
-  
+  const posts=[
+    {
+    _createdAt:new Date(),
+    views:55 ,
+    author:{_id:1,name:"Rahim"},
+    description:"this is a description",
+    image:"https://th.bing.com/th/id/OIP.MjdrtVOtV_Fp5yy_CgxXlAHaHa?w=1024&h=1024&rs=1&pid=ImgDetMain",
+    category:"Robots",
+    title:"we Robots",
+    _id:1
+  },
+]
 
   return (
        <>
@@ -22,7 +34,11 @@ export default async function Home({ searchParams }) {
       {query?`Search Result for "${query}"`:'All startups'}
         </p>
         <ul className="mt-7 card_grid">
-
+          {posts?.length>0?(
+            posts.map((post:StartupCardType,index:number)=>(
+              <StartupCard key={post?._id} post={post}></StartupCard>
+            ))
+          ):<p className="no-results">No startups found</p>}
         </ul>
       </section>
        </>
